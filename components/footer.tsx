7842d98e53d30305f4d57e2aafb984a72095230b
MTS4YOU <129235350+MTS4YOU@users.noreply.github.com>
2026-06-20 20:42:11 +0700

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -166,7 +166,7 @@ export function Footer() {
             <div className="flex flex-col sm:flex-row sm:items-center sm:gap-6 gap-4">
             <div className="flex space-x-6">
             </div>
-            <Button onClick="https://lynk.id/mtshosting/page/tokovps" className="bg-red-500 hover:bg-red-600 text-white">
+            <Button onClick="https://lynk.id/scriptnya/page/script-bot-free" className="bg-red-500 hover:bg-red-600 text-white">
               ⎙ 𝗖𝗟𝗜𝗖𝗞 𝗗𝗜 𝗦𝗜𝗡𝗜 ⎙
             </Button>
           </div>
